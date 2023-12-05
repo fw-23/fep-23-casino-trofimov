28562a4f2cd6615118ba65f3ed3b55950b7d476e
yuriy.trofimov <yytrofimov@yandex.ru>
2023-12-05 11:38:15 +0200

    REFACTOR: - old assets

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -4,22 +4,3 @@ import {getSession, isOpen, updateCurrentTime, updateOpenTimer as baseUpdateOpen
 window.addEventListener('load', function () {
     updateCurrentTime('current_time');
 });
-
-function updateOpenTimer(wasOpen, periodic = true) {
-    return baseUpdateOpenTimer(wasOpen, isOpen, onBecameOpen, onBecameClosed, onKeptClosed, periodic)
-}
-
-function onBecameClosed() {
-    let session = getSession()
-    if (!(session === null)) {
-        logout()
-    } else {
-        hideElementById('wrapper__reg_form')
-    }
-    showElementById('wrapper__closed')
-}
-
-function onBecameOpen() {
-    hideElementById('wrapper__closed')
-    showElementById('wrapper__reg_form')
-}
